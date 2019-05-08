85082c0d628ade60d360c4011bd719f6a8793f93
jorgeagudom <38132650+jorgeagudom@users.noreply.github.com>
2019-05-08 17:29:50 +0200

    Update InstalarIIS-DSC.ps1

--- InstalarIIS-DSC.ps1
+++ InstalarIIS-DSC.ps1
@@ -4,8 +4,17 @@
     {
         WindowsFeature IIS
         {
-        Ensure = "Present"
-        Name = "Web-Server"
+          Ensure = "Present"
+          Name = "Web-Server"
+          IncludeAllSubFeature = $true
         }
+        
+        File ArchivoIndex
+        {
+          Type = "File"
+          DestinationPath = "c:\inetpub\wwwroot\index.html"
+          Contents = "<H1> 'Estas viendo la web de MAT.com'</H1>"
+          Ensure = "Present"
+         }
     }
 }
